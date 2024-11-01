eb24bc16bed422a8e46255aa9e9cc52d30c91009
ushekate <ushekate@gmail.com>
2024-11-01 22:05:03 +0530

    bday wish

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+// document.getElementById("wish-button").addEventListener("click", function() {
+//     const surpriseMessage = document.getElementById("surprise-message");
+//     surpriseMessage.textContent = "Wishing you a day filled with love and joy! 🎉🎂";
+//     surpriseMessage.classList.remove("hidden");
+// });
